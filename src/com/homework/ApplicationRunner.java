48654e17f44242ae8126e38a2e0454cb4112085a
Max <max05012004@gmail.com>
2019-07-05 21:31:29 +0300

    2 variant to do

--- src/com/homework/ApplicationRunner.java
+++ src/com/homework/ApplicationRunner.java
@@ -1,12 +1,17 @@
 package com.homework;
 
 import com.homework.service.LogicSumming;
+import com.homework.service.NoMathLogicSumming;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
       LogicSumming logicSumming = new LogicSumming();
       logicSumming.mathSumming(12345);
 
+      System.out.println();
+
+      NoMathLogicSumming noMathLogicSumming = new NoMathLogicSumming();
+      noMathLogicSumming.noMathSumming(12345);
 
     }
 }
---
+++ src/com/homework/service/NoMathLogicSumming.java
@@ -0,0 +1,14 @@
+package com.homework.service;
+
+public class NoMathLogicSumming {
+    public void noMathSumming(int bigNumNoMath){
+        Integer Str = bigNumNoMath;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("");
+        stringBuilder.append(Str);
+        String Strr = stringBuilder.toString();
+        System.out.println(Strr);
+
+
+    }
+}
