dff41190c8046fe734bd29a0243fb9ec83e8f6f3
Max <max05012004@gmail.com>
2019-07-05 22:45:58 +0300

    Modern one-line summing

--- src/com/homework/service/NoMathLogicSumming.java
+++ src/com/homework/service/NoMathLogicSumming.java
@@ -1,14 +1,23 @@
 package com.homework.service;
 
+import java.util.Arrays;
+
 public class NoMathLogicSumming {
     public void change(int bigNumNoMath){
-        Integer.toString(bigNumNoMath);
         noMathSumming(String.valueOf(bigNumNoMath));
     }
 
     public void noMathSumming(String bigNumNoMath){
-        String.valueOf(bigNumNoMath);
         String stringNew = bigNumNoMath;
-        char[] m = stringNew.toCharArray();
+        // create chars array
+        char[] massiveChar = stringNew.toCharArray();
+
+        // create int array from existing char array
+        int[] massiveInt = new String(massiveChar).chars().toArray();
+        int sum = Arrays.stream(massiveInt).sum();
+
+        // output sum
+        System.out.println("Result no math summing " + sum);
+
     }
 }
